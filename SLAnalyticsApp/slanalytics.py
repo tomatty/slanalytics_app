ba72c8bbfe970f5ecca07ae7acd94d06abdf5581
takuya <81954921+tomatty@users.noreply.github.com>
2024-07-29 14:04:14 +0900

    Add files via upload

--- SLAnalyticsApp/slanalytics.py
+++ SLAnalyticsApp/slanalytics.py
@@ -2,7 +2,6 @@ import streamlit as st
 import importlib.util
 import os
 
-
 # 表示名リスト
 modules = ['Explanatory Data Analysis',
            'Feature Engineering',
@@ -19,10 +18,13 @@ module_paths = {
 module = st.sidebar.radio('Select Module', modules, index=0)
 
 # 選択されたファイルパスを取得
-module_path = os.path.join('appfile', module_paths[module])
+base_path = os.path.dirname(__file__)
+module_path = os.path.join(base_path, 'appfile', module_paths[module])
 
-# 選択されたファイルをインポートして実行
-if module_path:
+# ファイルの存在をチェック
+if os.path.exists(module_path):
     spec = importlib.util.spec_from_file_location("selected_module", module_path)
     selected_module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(selected_module)
+else:
+    st.error(f"No such file or directory: '{module_path}'")
